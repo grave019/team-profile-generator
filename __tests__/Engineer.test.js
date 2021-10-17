d93be5194fec2d5b00ed225df2f8452aa17277ea
Brent Graves <graves.brent@gmail.com>
2021-10-17 19:38:22 -0400

    added parameters for engineer object.

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -6,15 +6,22 @@ describe("Engineer", () => {
         const engineer = new Engineer();
         expect(typeof(engineer).tobe("object"));
 });
+    test("successfully create engineer object with all parameters", () => {
+        const engineer = new Engineer("fenix", 1127, "fenix@mail.com", "fenix01");
+        expect(typeof(engineer.name)).toBe("string");
+        expect(typeof(engineer.id)).toBe("number");
+        expect(typeof(engineer.email)).toBe("string");
+        expect(typeof(engineer.github)).tobe("string");
+    })
 
     test("Successfully sets a name via constructor arguements", () => {
-        const name = "Fenix";
+        const name = "fenix";
         const engineer = new Engineer(name);
         expect(engineer.name).tobe(name);
 });
 
     test("Successfully sets a id via constructor arguments", () =>{
-        const id = "1127";
+        const id = 1127;
         const engineer = new Engineer(id);
         expect(engineer.id).toBe(id);
 });
@@ -33,11 +40,11 @@ describe("Engineer", () => {
 
     test("Should get role of engineer using getRole", () =>{
         const engineer = "employee";
-        const engineer = new Engineer("Fenix", "1127", "fenix@mail.com", "fenix01");
+        const engineer = new Engineer("Fenix", 1127, "fenix@mail.com", "fenix01");
         expect (engineer.getRole()).toBe("Engineer");
 });
     test("Should get the github username using the getGithub()", () => {
-        const engineer = new Engineer("Fenix", "1127", "fenix@mail.com", "fenix01");
+        const engineer = new Engineer("Fenix", 1127, "fenix@mail.com", "fenix01");
         expect (engineer.getGithub()).toBe("fenix01")
     });
 })
