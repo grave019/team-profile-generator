e4c9861a8cc8c6ca726b988e56cfe3030c5d9ec5
Brent Graves <graves.brent@gmail.com>
2021-10-17 12:31:45 -0400

    added test for employee.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,6 +1,6 @@
 const Employee = require('../lib/Employee');
 
-test("seccessfully instantiates instance of Employee Class", () => {
+test("successfully instantiates instance of Employee Class", () => {
     const employee = new Employee();
     expect(typeof(employee).toBe("object"));
 })
@@ -9,4 +9,14 @@ test("Successfully set a name via constructor arguments", () => {
     const name = "Brent";
     const employee = new Employee(name);
     expect(employee.name).toBe(name);
+})
+test("successfully set a id via constructor arguments", () => {
+    const id = "0531";
+    const employee = new Employee(id);
+    expect(employee.id).toBe(id);
+})
+test("successfully set a email via constructor arguments", () =>{
+    const email = "brent@mail.com";
+    const employee = new Employee(email);
+    expect (employee.email).toBe(email);
 })
