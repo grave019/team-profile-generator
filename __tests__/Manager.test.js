1feae0cced940df032ca37875a2e6d7265ba2f55
Brent Graves <graves.brent@gmail.com>
2021-10-17 18:48:43 -0400

    added test for manager.

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,41 @@
+const Manager = require("../lib/Manager");
+
+describe("Manager", () => {
+
+    test("successfully instantiates instance of Manager Class", () => {
+        const manager = new Manager();
+        expect(typeof(manager).toBe("object"));
+});
+
+    test("Successfully set a name via constructor arguments", () => {
+        const name = "Brent";
+        const manager = new Manager(name);
+        expect(manager.name).toBe(name);
+});
+    test("successfully set a id via constructor arguments", () => {
+        const id = "0531";
+        const manager = new Manager(id);
+        expect(manager.id).toBe(id);
+});
+    test("successfully set a email via constructor arguments", () =>{
+        const email = "brent@mail.com";
+        const manager = new Manager(email);
+        expect (manager.email).toBe(email);
+});
+    test("should get the role of the new Manager", () => {
+        const manager = new Manager("brent", "0531", "brent@mail.com");
+        expect(manager.getRole()).toBe("Manager");
+});
+    test("should get the name of manager using getName", () => {
+        const manager = new Manager("brent", "0531", "brent@mail.com");
+        expect(manager.getName()).toBe("brent");
+    });
+    test("should get the id of manager using getId", () => {
+        const manager = new Manager("brent", "0531", "brent@mail.com");
+        expect(manager.getId()).toBe("0531");
+    });
+    test("should get the email of manager using getEmail", () => {
+        const manager = new Manager("brent", "0531", "brent@mail.com");
+        expect(manager.getEmail()).toBe("brent@mail.com");
+    });
+});
